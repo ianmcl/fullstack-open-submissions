a362af9abd53cc90ad52d4a36f0768573accba58
Ian McLaughlin <1479726+ianmcl@users.noreply.github.com>
2025-09-20 13:24:16 -0400

    1.11*: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,11 +10,13 @@ const Button = ({ name, onClick }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text}: {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
+
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
@@ -26,14 +28,16 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="total" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + " %"} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="total" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
   )
 }
 
@@ -43,12 +47,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
   return (
     <div>
       <Header name="give feedback" />
-      <Button name="good" onClick={() => setGood(good + 1)} />
-      <Button name="neutral" onClick={() => setNeutral(neutral + 1)} />
-      <Button name="bad" onClick={() => setBad(bad + 1)} />
+      <Button name="good" onClick={handleGoodClick} />
+      <Button name="neutral" onClick={handleNeutralClick} />
+      <Button name="bad" onClick={handleBadClick} />
       <Header name="statistics" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
