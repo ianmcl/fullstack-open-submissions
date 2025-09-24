9606ff5d4596e3a31833be1f1bcd5c128aaf599d
Ian McLaughlin <1479726+ianmcl@users.noreply.github.com>
2025-09-24 12:55:05 -0400

    Update phonebook.js
    optimized

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,29 +1,21 @@
 import axios from 'axios'
+
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return axios.get(baseUrl).then(response => response.data)
 }
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then((response) => response.data)
+const create = (newPerson) => {
+  return axios.post(baseUrl, newPerson).then(response => response.data)
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then((response) => response.data)
+  return axios.put(`${baseUrl}/${id}`, newObject).then((response) => response.data)
 }
 
 const deletePerson = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then((response) => response.data)
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
 }
 
-export default {
-  getAll,
-  create,
-  update,
-  deletePerson
-}
+export default { getAll, create, update, deletePerson }
