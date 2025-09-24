598d1fdf1c96685df0bb333c2a0a1667206f3a5e
Ian McLaughlin <1479726+ianmcl@users.noreply.github.com>
2025-09-24 18:20:32 -0400

    3.2: Phonebook backend step 2

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const count = persons.length
+  const date = new Date()
+
+  res.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${date}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
