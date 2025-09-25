68a181c600bea45c72abfa96aa2f72f45d067c14
Ian McLaughlin <1479726+ianmcl@users.noreply.github.com>
2025-09-25 11:33:25 -0400

    3.11 Full Stack Phonebook
    CSS to file for error

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,20 +1,12 @@
+import './Notification.css'
+
 const Notification = ({ message, type }) => {
   if (message === null) {
     return null
   }
 
-  const notificationStyle = {
-    color: type === 'error' ? 'red' : 'green',
-    background: 'lightgrey',
-    fontSize: 20,
-    borderStyle: 'solid',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-  }
-
   return (
-    <div style={notificationStyle}>
+    <div className={`notification ${type}`}>
       {message}
     </div>
   )
